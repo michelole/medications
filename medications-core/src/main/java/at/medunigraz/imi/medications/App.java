fde7d6ecfd44493edb3fd85c18de6c238878073d
Michel Oleynik <michel.oleynik@stud.medunigraz.at>
2015-11-18 00:17:12 +0100

    Initial commit

---
+++ medications-core/src/main/java/at/medunigraz/imi/medications/App.java
@@ -0,0 +1,13 @@
+package at.medunigraz.imi.medications;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
